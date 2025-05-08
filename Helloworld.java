d95d8278ec26866317ae8f7c4689704dd491fdfd
MicheleTaglia <michele.tagliafierro@capellinisauro.edu.it>
2025-05-08 06:47:21 +0000

    aggiunta stampa da vector

--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,16 @@ import java.util.*;
 
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("helloworld");
-    }
+        System.out.println("hello Taglia");
+
+        Vector<Character> lettere = new Vector<Character>();
+        lettere.add('T');
+        lettere.add('A');
+        lettere.add('G');
+        lettere.add('L');
+        lettere.add('I');
+        lettere.add('A');
+        for (Character lettera : lettere) {
+            System.out.println(lettera);
+        }
 }
