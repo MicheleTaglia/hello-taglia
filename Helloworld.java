83e8aa4db4c858d58c0b08f156d2611654031431
MicheleTaglia <michele.tagliafierro@capellinisauro.edu.it>
2025-05-08 06:41:40 +0000

    aggiunta stampa da array statico

--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+       System.out.println("Hello Taglia");
+
+       char[] lettere = {'T' , 'A' , 'G' , 'L' , 'I' , 'A'};
+       System.out.println(lettere);
+
+       for(char lettera : lettere){
+           System.out.println(lettera);
+       }
     }
-}
