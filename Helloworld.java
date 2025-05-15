338191878b130bcceb2f1acd0d8e884de2920508
iurim159 <hacking1580@gmail.com>
2025-05-15 07:09:10 +0000

    aggiunta del codice da parte di Iurim

--- Helloworld.java
+++ Helloworld.java
@@ -2,11 +2,21 @@ import java.util.*;
 
 public class Helloworld {
     public static void main(String[] args) {
+
+        System.out.println(" _   _      _ _         __        __         _     _ ");
+        System.out.println("| | | | ___| | | ___    \\ \\      / /__  _ __| | __| |");
+        System.out.println("| |_| |/ _ \\ | |/ _ \\    \\ \\ /\\ / / _ \\| '__| |/ _` |");
+        System.out.println("|  _  |  __/ | | (_) |    \\ V  V / (_) | |  | | (_| |");
+        System.out.println("|_| |_|\\___|_|_|\\___( )    \\_/\\_/ \\___/|_|  |_|\\__,_|");
+        System.out.println("                     |/                               ");
+
         for (int i = 0; i < 3; i++) {
             System.out.println("helloTaglia");
         }
         System.out.println("16.04.2007");
         System.out.println("Forza Follo Calcio!");
+        System.out.println("Forza Follo Calcio!");
+
     }
 }
 
